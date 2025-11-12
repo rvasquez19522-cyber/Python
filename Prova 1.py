6120adfd7f87ca55dae349c6746b7c316c8a2df2
rvasquez19522-cyber <rvasquez19522@iesjoanramis.org>
2025-11-12 13:40:19 +0100

    ex 1.b

--- Prova 1.py
+++ Prova 1.py
@@ -4,4 +4,11 @@ d= float(input("nombre"))
 c= a + b + d
 c = a + b + d
 print("El resultat de la suma {} + {} + {} és {}".format(a, b, d, c))
+num = int(input("Introdueix un número: "))
+if num > 20:
+        print("El número és major que 20.")
+elif num < 20:
+        print("El número és menor que 20.")
+else:
+        print("El número és igual a 20.")
 
