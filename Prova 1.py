b93cea9d17a462586dcf2dee6a3913c8917f0ed6
rvasquez19522-cyber <rvasquez19522@iesjoanramis.org>
2025-11-12 13:18:07 +0100

    1r ejercicio de python

---
+++ Prova 1.py
@@ -0,0 +1,7 @@
+a = float(input("Escriure el primer nombre:"))
+b = float(input("Escriure el segon nombre: "))
+d= float(input("nombre"))
+c= a + b + d
+c = a + b + d
+print("El resultat de la suma {} + {} + {} és {}".format(a, b, d, c))
+
