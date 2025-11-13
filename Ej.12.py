b1fe9894dcaf5f150340296dbd9606478a755105
rvasquez19522-cyber <rvasquez19522@iesjoanramis.org>
2025-11-13 11:56:27 +0100

    Ej12B

--- Ej.12.py
+++ Ej.12.py
@@ -1,7 +1,12 @@
+def majoredat(edat):
+    if edat > 18: 
+        print("és major d'edat")
+    elif edat<18: 
+        print("és menor d'edat")
+    else:
+        print("felicitats, ets major d'edat")
+
 edat = int(input("escriu la seva edat: "))
-if edat > 18: 
-    print("és major d'edat")
-elif edat<18: 
-    print("és menor d'edat")
-else:
-    print("felicitats, ets major d'edat")
+majoredat(edat)
+edat = int(input("escriu la seva edat: "))
+majoredat(edat)
